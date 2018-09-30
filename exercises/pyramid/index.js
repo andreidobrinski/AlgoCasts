18a38d4c1f8c6d4d980f7e8ed58ac4facc9c751f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-09-30 12:24:56 -0400

    pyramid - recursive solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,7 +14,29 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
+
+function pyramidOne(n) {
   // calculate mid point of the row
   const midpoint = Math.floor((2 * n - 1) / 2);
 
