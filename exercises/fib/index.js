8c306081957c648b66d5d198eea6c0167290f392
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-10-08 18:20:04 -0400

    fibonacci - memoize

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,17 +8,34 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+const fib = memoize(slowfib);
+
+// recursive solution has exponential runtime complexity
+// exponential runtime is the least ideal for a solution
+// a better recursive solution would involve memoization
+
+function slowfib(n) {
   if (n < 2) {
     return n;
   }
 
   return fib(n - 1) + fib(n - 2);
 }
-
-// recursive solution has exponential runtime complexity
-// exponential runtime is the least ideal for a solution
-// a better recursive solution would involve memoization
 
 function fibOne(n) {
   const result = [0, 1];
