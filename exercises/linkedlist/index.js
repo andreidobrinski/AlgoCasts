49c599577d537693abdbc4a77e7d8f5ca20c5221
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-10-15 18:00:14 -0400

    add insertLast method to linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -83,6 +83,18 @@ class LinkedList {
 
     previous.next = null;
   }
+
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      // there are some existing nodes
+      last.next = new Node(data);
+    } else {
+      // the chain is empty
+      this.head = new Node(data);
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
