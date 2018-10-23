85ae68c29c3a07b440f10089dd37eb952f40919f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-10-23 17:54:03 -0400

    add selectionSort algorithm

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -19,8 +19,26 @@ function bubbleSort(arr) {
 
 // difficulty: easier
 // worst-case runtime: n^2
+// "prove-me-wrong" algorithm, b/c we assume that "i" is the smaller number
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
 
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+
+  return arr;
 }
 
 // difficulty: medium
