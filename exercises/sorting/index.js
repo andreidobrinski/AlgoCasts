ac25ee21d9ef25084c074b089e2cd8c3ba78d594
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-10-23 17:38:06 -0400

    add difficulty and runtime to sorting

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,14 +1,20 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+// difficulty: easiest
+// worst-case runtime: n^2
 function bubbleSort(arr) {
 
 }
 
+// difficulty: easier
+// worst-case runtime: n^2
 function selectionSort(arr) {
 
 }
 
+// difficulty: medium
+// worst-case runtime: n*log(n)
 function mergeSort(arr) {
 
 }
