ef9c3dbbadc2d0bd9517c7283410a1ff3dc46668
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-10-23 17:46:52 -0400

    add bubble sorting algorithm

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -4,7 +4,17 @@
 // difficulty: easiest
 // worst-case runtime: n^2
 function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i -1); j++) {
+      if (arr[j] > arr[j+1]) {
+        const lesser = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
 
+  return arr;
 }
 
 // difficulty: easier
